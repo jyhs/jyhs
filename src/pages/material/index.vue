0a348ed90bd77aa8b1a79cef5896acfa34887a08
jyhs <jyhs_123@163.com>
2019-02-02 03:41:55 +0800

    update css

--- src/pages/material/index.vue
+++ src/pages/material/index.vue
@@ -625,6 +625,7 @@ button {
   background: url(https://static.huanjiaohu.com/mini/index/baike_contents_bg.jpg) bottom no-repeat;
   background-size: 100%;
   border-radius: 6px;
+  box-shadow: 0 2px 8px #ebebeb;
   color:#000;line-height: 20px;margin:0 10px;border: solid 1px #acdcfc;}
 .material_all > .descrition .wxParse{padding:8px;width: auto;color:#000;padding-bottom:110px}
 .bottom-btn .fenxiang{background-color:#87d74f;border:none;border-radius: 0;border:none}
